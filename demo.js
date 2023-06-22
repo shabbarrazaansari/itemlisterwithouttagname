dce3241f1eacb0d6627ba5275561fa7f0970ba99
shabbarrazaansari <shabbaransari98@gmail.com>
2023-06-22 08:33:10 +0530

    item lister

---
+++ demo.js
@@ -0,0 +1,8 @@
+// // Headers.style.color=black;
+// var headertitle = document.getElementById("header-title");
+// headertitle.style.borderBottom= "solid,3px,black";
+// Get the element with id "header-title"
+var headerTitle = document.getElementById("header-title");
+
+// Add a border to the bottom of the element
+headerTitle.style.borderBottom = "1px solid black";
