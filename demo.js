f717e95883952cf5f954f003043dc56d890ac85e
shabbarrazaansari <shabbaransari98@gmail.com>
2023-06-22 09:04:10 +0530

    listitems by class name

--- demo.js
+++ demo.js
@@ -2,7 +2,17 @@
 // var headertitle = document.getElementById("header-title");
 // headertitle.style.borderBottom= "solid,3px,black";
 // Get the element with id "header-title"
-var headerTitle = document.getElementById("header-title");
+// var headerTitle = document.getElementById("header-title");
 
 // Add a border to the bottom of the element
-headerTitle.style.borderBottom = "1px solid black";
+// headerTitle.style.borderBottom = "1px solid black";
+// Select the elements by class name
+var listItems = document.getElementsByClassName('list-group-item');
+
+// Make the 3rd element have a green background color
+listItems[2].style.backgroundColor = 'green';
+
+// Make all elements have bold font weight
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontWeight = 'bold';
+}
