b47782b1260add4da30eed38907000c2af564ef4
shabbarrazaansari <shabbaransari98@gmail.com>
2023-06-22 16:13:47 +0530

    font color to green

--- demo.js
+++ demo.js
@@ -16,4 +16,16 @@
 // for (var i = 0; i < listItems.length; i++) {
 //   listItems[i].style.fontWeight = 'bold';
 // }
+var items = document.querySelectorAll("#items li");
+
+// Change font color of the second item and background color of all odd items to green
+if (items.length >= 2) {
+  items[1].style.color = "green";
+}
+
+for (var i = 0; i < items.length; i++) {
+  if (i % 2 !== 0) {
+    items[i].style.backgroundColor = "green";
+  }
+}
 
