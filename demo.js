6728439f016b0613da878bc13183c21ccb221501
shabbarrazaansari <shabbaransari98@gmail.com>
2023-06-24 09:30:54 +0530

    itemlister dom2nd part

--- demo.js
+++ demo.js
@@ -15,17 +15,38 @@
 // Make all elements have bold font weight
 // for (var i = 0; i < listItems.length; i++) {
 //   listItems[i].style.fontWeight = 'bold';
+// // }
+// var items = document.querySelectorAll("#items li");
+
+// // Change font color of the second item and background color of all odd items to green
+// if (items.length >= 2) {
+//   items[1].style.color = "green";
 // }
-var items = document.querySelectorAll("#items li");
 
-// Change font color of the second item and background color of all odd items to green
-if (items.length >= 2) {
-  items[1].style.color = "green";
-}
+// for (var i = 0; i < items.length; i++) {
+//   if (i % 2 !== 0) {
+//     items[i].style.backgroundColor = "green";
+//   }
+// // }
+var itemlist=document.querySelector('#items');
+// console.log(itemlist);
+// console.log(itemlist.parentElement);
+// itemlist.parentElement.style.backgroundColor="grey";
+// // console.log(itemlist.lastElementChild);
+// console.log(itemlist.firstElementChild);
+// itemlist.firstElementChild.style.backgroundColor="grey";
+// console.log(itemlist.nextSibling);
+// console.log(itemlist.nextElementSibling);
+var newDiv = document.createElement("div");
+newDiv.className = "hello";
+newDiv.id = 'hello1';
+newDiv.setAttribute('title', 'hello div');
 
-for (var i = 0; i < items.length; i++) {
-  if (i % 2 !== 0) {
-    items[i].style.backgroundColor = "green";
-  }
-}
+var newDivText = document.createTextNode('hello world');
+newDiv.appendChild(newDivText);
 
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+container.insertBefore(newDiv, h1);
